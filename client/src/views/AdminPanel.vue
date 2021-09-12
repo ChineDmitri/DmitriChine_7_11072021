3114d8d757ea9bac15a28cd8915e63ebb5db0f16
ChineDmitri <mr.dima11@gmail.com>
2021-09-13 00:02:01 +0200

    Changement du profil avec drag&drop

--- client/src/views/AdminPanel.vue
+++ client/src/views/AdminPanel.vue
@@ -1,4 +1,6 @@
 <script>
+import { sendRequest } from "../helpers/sendRequest.js";
+
 export default {
   name: "AdminPanel",
 
@@ -6,12 +8,11 @@ export default {
 
   data() {
     return {
-      users: [
-        { id: 1, name: "Dima1" },
-        { id: 2, name: "Dima2" },
-        { id: 3, name: "Dima3" }
-      ],
-      item: NaN
+      users: [],
+      moderators: [],
+      admins: [],
+      item: NaN,
+      loading: false
     };
   },
 
@@ -23,8 +24,8 @@ export default {
       setTimeout(() => event.target.classList.add("hide"), 1);
     },
     dragend(event) {
-      console.log("DRAG end", event.target);
-      console.log("moder user", parseInt(event.target.id));
+      // console.log("DRAG end", event.target);
+      // console.log("moder user", parseInt(event.target.id));
 
       this.item = event.target;
     },
@@ -60,16 +61,46 @@ export default {
             stringClasses.length - 1
           );
 
-          switch (event.target.classList.value) {
-            case "admins":
-              console.log("admin", this.item.id);
-              break;
-            case "moderators":
-              console.log("moderator", this.item.id);
-              break;
-            case "users":
-              console.log("user", this.item.id);
-              break;
+          if (event.target.classList.value == "admins") {
+            console.log("admin switch", parseInt(this.item.id, 10));
+
+            const body = {
+              memberIdChange: parseInt(this.item.id, 10),
+              profilChange: "a"
+            };
+
+            sendRequest("http://localhost:3000/api/admin/profil/", "PUT", body);
+            // .this(res => {
+            //   console.log(res);
+            // })
+            // .catch(err => console.log(err));
+          }
+
+          if (event.target.classList.value == "moderators") {
+            console.log("moderator switch", parseInt(this.item.id, 10));
+
+            const body = {
+              memberIdChange: parseInt(this.item.id, 10),
+              profilChange: "m"
+            };
+
+            sendRequest("http://localhost:3000/api/admin/profil/", "PUT", body);
+            // .this(res => {
+            //   this.loading = false;
+            //   console.log(res);
+            // })
+            // .catch(err => console.log(err));
+          }
+
+          if (event.target.classList.value == "users") {
+            console.log("user switch", parseInt(this.item.id, 10));
+
+            const body = {
+              memberIdChange: parseInt(this.item.id, 10),
+              profilChange: "u"
+            };
+
+            sendRequest("http://localhost:3000/api/admin/profil/", "PUT", body);
           }
 
           // on garder seulement la classe ajoutée
@@ -77,18 +108,41 @@ export default {
         }
         // console.log("TARGET", event.target.classList.value);
       }, 100);
-    },
-    // DRAG&DROP - end
-
-    eleve(idx) {
-      console.log(idx);
     }
+    // DRAG&DROP - end
+  },
+
+  // avant Mount tri massive par cetegorie
+  beforeMount() {
+    sendRequest("http://localhost:3000/api/admin/users/", "GET")
+      .then(members => {
+        if (members.error === 0) {
+          this.$router.push("/");
+        } else {
+          members.forEach(member => {
+            switch (member.profil) {
+              case "u":
+                this.users.push(member);
+                break;
+              case "m":
+                this.moderators.push(member);
+                break;
+              case "a":
+                this.admins.push(member);
+                break;
+            }
+          });
+        }
+      })
+      .catch(err => console.log(err));
   }
 };
 </script>
 
 <template>
   <div id="panel">
+    <div class="loading" v-if="loading"></div>
+
     <div
       class="admins"
       v-on:dragover="dragover"
@@ -98,15 +152,17 @@ export default {
     >
       <h2>Administrateurs</h2>
       <div
-        v-for="user of users"
-        v-bind:key="user.id"
-        v-bind:id="user.id"
+        v-for="admin of admins"
+        v-bind:key="admin.id"
+        v-bind:id="admin.id"
         v-on:dragstart="dragstart"
         v-on:dragend="dragend"
-        class="user"
+        class="admin"
         draggable="true"
       >
-        <span class="data">id: {{ user.id }} | pseudo: {{ user.name }} </span>
+        <span class="data">
+          id: {{ admin.id }} | pseudo: {{ admin.pseudo }}
+        </span>
         <span>
           <button class="desabled">
             <i class="fas fa-circle"></i>
@@ -129,6 +185,30 @@ export default {
       v-on:dragenter="dragenter"
     >
       <h2>Moderateurs</h2>
+      <div
+        v-for="moderator of moderators"
+        v-bind:key="moderator.id"
+        v-bind:id="moderator.id"
+        v-on:dragstart="dragstart"
+        v-on:dragend="dragend"
+        class="moderator"
+        draggable="true"
+      >
+        <span class="data">
+          id: {{ moderator.id }} | pseudo: {{ moderator.pseudo }}
+        </span>
+        <span>
+          <button class="desabled">
+            <i class="fas fa-circle"></i>
+          </button>
+          <button>A</button>
+          <button>M</button>
+          <button>U</button>
+          <button class="delete">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </span>
+      </div>
     </div>
 
     <div
@@ -138,12 +218,45 @@ export default {
       v-on:dragleave="dragleave"
       v-on:dragenter="dragenter"
     >
-      <h2>Utilisateurs</h2>
+      <h2>Utulisateurs</h2>
+      <div
+        v-for="user of users"
+        v-bind:key="user.id"
+        v-bind:id="user.id"
+        v-on:dragstart="dragstart"
+        v-on:dragend="dragend"
+        class="user"
+        draggable="true"
+      >
+        <span class="data">
+          id: {{ user.id }} | pseudo: {{ user.pseudo }}
+        </span>
+        <span>
+          <button class="desabled">
+            <i class="fas fa-circle"></i>
+          </button>
+          <button>A</button>
+          <button>M</button>
+          <button>U</button>
+          <button class="delete">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+.loading {
+  opacity: 0.5;
+  position: absolute;
+  top: -20px;
+  width: 100vw;
+  height: calc(100% + 40px);
+  background-color: black;
+  z-index: 99;
+}
 h2 {
   margin: 1rem 1rem 0;
 }
